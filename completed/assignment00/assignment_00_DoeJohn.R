4e6ca7dfe5b7d7687dff6272890a3e90817dee0f
Shawn Hermans <shawnhermans@gmail.com>
2020-04-29 00:07:08 -0500

    Add files for assignment 0

---
+++ completed/assignment00/assignment_00_DoeJohn.R
@@ -0,0 +1,53 @@
+# Assignment: ASSIGNMENT 0
+# Name: Doe, John
+# Date: 2020-01-01
+
+# Basics
+
+## Add 8 and 5
+8 + 5
+
+## Subtract 6 from 22
+22 - 6
+
+## Multiply 6 by 7
+6 * 7
+
+## Add 4 to 6 and divide the result by 2
+(4 + 6) / 2
+
+## Compute 5 modulo 2
+5 %% 2
+
+## Assign the value 82 to the variable x
+## Print x
+x <- 82
+x
+
+## Assign the value 41 to the variable y
+## Print y
+y <- 41
+
+## Assign the output of x + y to the variable z
+## Print z
+z <- x + y
+z
+
+## Assign the string value "DSC520" to the variable class_name
+## Print the value of class_name
+class_name <- "DSC520"
+class_name
+
+## Assign the string value of TRUE to the variable is_good
+## Print the value of is_good
+is_good <- TRUE
+is_good
+
+## Check the class of the variable is_good using the class() function
+class(is_good)
+
+## Check the class of the variable z using the class() function
+class(z)
+
+## Check the class of the variable class_name using the class() function
+class(class_name)
