477b2a19ea44953781301ef339529b6a218af6fe
Shawn Hermans <shawnhermans@gmail.com>
2020-04-29 01:03:40 -0500

    Add assignment 5

---
+++ assignments/assignment05/assignment_05_LastnameFirstname.R
@@ -0,0 +1,25 @@
+# Assignment: ASSIGNMENT 5
+# Name: Lastname, Firstname
+# Date: 2010-02-14
+
+## Set the working directory to the root of your DSC 520 directory
+setwd("/home/jdoe/Workspaces/dsc520")
+
+## Load the `data/r4ds/heights.csv` to
+heights_df <- read.csv("data/r4ds/heights.csv")
+
+## Using `cor()` compute correclation coefficients for
+## height vs. earn
+___
+### age vs. earn
+___
+### ed vs. earn
+___
+
+## Spurious correlation
+## The following is data on US spending on science, space, and technology in millions of today's dollars
+## and Suicides by hanging strangulation and suffocation for the years 1999 to 2009
+## Compute the correlation between these variables
+tech_spending <- c(18079, 18594, 19753, 20734, 20831, 23029, 23597, 23584, 25525, 27731, 29449)
+suicides <- c(5427, 5688, 6198, 6462, 6635, 7336, 7248, 7491, 8161, 8578, 9000)
+___
