0d2c1bc68206af5f49b8148b1ec79f5a8cd83673
jjbrough95 <49848157+jjbrough95@users.noreply.github.com>
2020-09-15 20:48:36 -0600

    assignment 02: Brough Jed

--- assignments/assignment02/assignment_02_LastnameFirstname.R
+++ assignments/assignment02/assignment_02_LastnameFirstname.R
@@ -1,40 +1,40 @@
 # Assignment: ASSIGNMENT 2
-# Name: Lastname, Firstname
-# Date: 2010-02-14
+# Name: Brough, Jed
+# Date: 2020-09-15
 
 ## Check your current working directory using `getwd()`
-___
+getwd()
 
 ## List the contents of the working directory with the `dir()` function
-___
+dir()
 
 ## If the current directory does not contain the `data` directory, set the
 ## working directory to project root folder (the folder should contain the `data` directory
 ## Use `setwd()` if needed
-setwd("/home/jdoe/Workspaces/dsc520")
+setwd("/Users/jedediahbrough/Documents/Data")
 
 ## Load the file `data/tidynomicon/person.csv` to `person_df1` using `read.csv`
 ## Examine the structure of `person_df1` using `str()`
-person_df1 <- ___
-___
+person_df1 <- read.csv("data/tidynomicon/person.csv")
+str(person_df1)
 
 ## R interpreted names as factors, which is not the behavior we want
 ## Load the same file to person_df2 using `read.csv` and setting `stringsAsFactors` to `FALSE`
 ## Examine the structure of `person_df2` using `str()`
-person_df2 <- ___
-___
+person_df2 <- read.csv("data/tidynomicon/person.csv", stringsAsFactors = FALSE)
+str(person_df2)
 
 ## Read the file `data/scores.csv` to `scores_df`
 ## Display summary statistics using the `summary()` function
-scores_df <- ___
-___
+scores_df <- read.csv("data/scores.csv")
+summary(scores_df)
 
 ## Load the `readxl` library
-library(___)
+library(readxl)
 
 ## Using the excel_sheets() function from the `readxl` package,
 ## list the worksheets from the file `data/G04ResultsDetail2004-11-02.xls`
-___
+excel_sheets("data/G04ResultsDetail2004-11-02.xls")
 
 ## Using the `read_excel` function, read the Voter Turnout sheet
 ## from the `data/G04ResultsDetail2004-11-02.xls`
@@ -42,8 +42,8 @@ ___
 ## The header is in the second row, so make sure to skip the first row
 ## Examine the structure of `voter_turnout_df1` using `str()`
 
-voter_turnout_df1 <- ___
-___
+voter_turnout_df1 <- read_excel("data/G04ResultsDetail2004-11-02.xls", skip = 1)
+str(voter_turnout_df1)
 
 ## Using the `read_excel()` function, read the Voter Turnout sheet
 ## from `data/G04ResultsDetail2004-11-02.xls`
@@ -51,43 +51,44 @@ ___
 ## Use the names "ward_precint", "ballots_cast", "registered_voters", "voter_turnout"
 ## Assign the data to the `voter_turnout_df2`
 ## Examine the structure of `voter_turnout_df2` using `str()`
-voter_turnout_df2 <- ___
-___
+voter_turnout_df2 <- read_excel("data/G04ResultsDetail2004-11-02.xls", skip = 2)
+colnames(voter_turnout_df2) <- c("ward_precint","ballots_cast","registered_voters","voter_turnout")
+str(voter_turnout_df2)
 
 ## Load the `DBI` library
-___
+library(DBI)
 
 ## Create a database connection to `data/tidynomicon/example.db` using the dbConnect() function
 ## The first argument is the database driver which in this case is `RSQLite::SQLite()`
 ## The second argument is the path to the database file
 ## Assign the connection to `db` variable
-db <- ___
+db <- dbConnect(RSQLite::SQLite(), dbname = "tidynomicon/example.db")
 
 ## Query the Person table using the `dbGetQuery` function and the
 ## `SELECT * FROM PERSON;` SQL statement
 ## Assign the result to the `person_df` variable
 ## Use `head()` to look at the first few rows of the `person_df` dataframe
-person_df <- ___
-___
+person_df <- dbGetQuery("SELECT * FROM PERSON")
+head(person_df)
 
 ## List the tables using the `dbListTables()` function
 ## Assign the result to the `table_names` variable
-table_names <- ____
+table_names <- dbListTables(person_df)
 
 ## Read all of the tables at once using the `lapply` function and assign the result to the `tables` variable
 ## Use `table_names`, `dbReadTable`, and `conn = db` as arguments
 ## Print out the tables
-tables <- ____
+tables <- lapply("table_names", "dbReadTable", "conn = db")
 tables
 
 ## Use the `dbDisconnect` function to disconnect from the database
-___
+dbDisconnect()
 
 ## Import the `jsonlite` library
-library(___)
+library(jsonlite)
 
 ## Convert the scores_df dataframe to JSON using the `toJSON()` function
-___
+toJSON(scores_df)
 
 ## Convert the scores dataframe to JSON using the `toJSON()` function with the `pretty=TRUE` option
-___
+toJSON(scores_df, pretty = TRUE)
