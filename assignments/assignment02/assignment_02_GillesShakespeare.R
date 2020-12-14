90567d4bc5e14199a829af995004a64498542609
shakesjg <50377809+shakesjg@users.noreply.github.com>
2020-12-13 19:19:49 -0500

    Update assignment_02_GillesShakespeare.R

--- assignments/assignment02/assignment_02_GillesShakespeare.R
+++ assignments/assignment02/assignment_02_GillesShakespeare.R
@@ -6,7 +6,6 @@
 ## Check your current working directory using `getwd()`
 getwd()
 
-
 ## List the contents of the working directory with the `dir()` function
 dir()
 
@@ -15,19 +14,16 @@ dir()
 ## Use `setwd()` if needed
 setwd("/Users/Shakes/Desktop/DSC520/data")
 
-
 ## Load the file `data/tidynomicon/person.csv` to `person_df1` using `read.csv`
 ## Examine the structure of `person_df1` using `str()`
 person_df1 <- read.csv("tidynomicon/person.csv")
 str(person_df1)
-
 
 ## R interpreted names as factors, which is not the behavior we want
 ## Load the same file to person_df2 using `read.csv` and setting `stringsAsFactors` to `FALSE`
 ## Examine the structure of `person_df2` using `str()`
 person_df2 <- read.csv("tidynomicon/person.csv", stringsAsFactors = FALSE)
 str(person_df2)
-
 
 ## Read the file `data/scores.csv` to `scores_df`
 ## Display summary statistics using the `summary()` function
@@ -38,11 +34,9 @@ summary(scores_df)
 ## Load the `readxl` library
 library(readxl)
 
-
 ## Using the excel_sheets() function from the `readxl` package,
 ## list the worksheets from the file `data/G04ResultsDetail2004-11-02.xls`
 excel_sheets("G04ResultsDetail2004-11-02.xls") 
-
 
 ## Using the `read_excel` function, read the Voter Turnout sheet
 ## from the `data/G04ResultsDetail2004-11-02.xls`
@@ -51,9 +45,6 @@ excel_sheets("G04ResultsDetail2004-11-02.xls")
 ## Examine the structure of `voter_turnout_df1` using `str()`
 voter_turnout_df1 <- read_excel("G04ResultsDetail2004-11-02.xls",sheet = "Voter Turnout",skip=1)
 str(voter_turnout_df1)
-
-
-
 
 ## Using the `read_excel()` function, read the Voter Turnout sheet
 ## from `data/G04ResultsDetail2004-11-02.xls`
@@ -64,17 +55,14 @@ str(voter_turnout_df1)
 voter_turnout_df2 <- read_excel("G04ResultsDetail2004-11-02.xls", sheet = "Voter Turnout",skip=2,col_names = c("ward_precint", "ballots_cast", "registered_voters", "voter_turnout"))
 str(voter_turnout_df2)
 
-
 ## Load the `DBI` library
 library(DBI)
-
 
 ## Create a database connection to `data/tidynomicon/example.db` using the dbConnect() function
 ## The first argument is the database driver which in this case is `RSQLite::SQLite()`
 ## The second argument is the path to the database file
 ## Assign the connection to `db` variable
 db <-dbConnect(RSQLite::SQLite(),"tidynomicon/example.db")
-
 
 ## Query the Person table using the `dbGetQuery` function and the
 ## `SELECT * FROM PERSON;` SQL statement
@@ -85,9 +73,7 @@ head(person_df)
 
 ## List the tables using the `dbListTables()` function
 ## Assign the result to the `table_names` variable
-table_names <- ____
 table_names <-dbListTables(db)
-
 
 ## Read all of the tables at once using the `lapply` function and assign the result to the `tables` variable
 ## Use `table_names`, `dbReadTable`, and `conn = db` as arguments
@@ -95,18 +81,14 @@ table_names <-dbListTables(db)
 tables <- lapply(table_names, dbReadTable, conn=db)
 tables
 
-
 ## Use the `dbDisconnect` function to disconnect from the database
 dbDisconnect(db)
-
 
 ## Import the `jsonlite` library
 library(jsonlite)
 
-
 ## Convert the scores_df dataframe to JSON using the `toJSON()` function
 toJSON(scores_df)
-
 
 ## Convert the scores dataframe to JSON using the `toJSON()` function with the `pretty=TRUE` option
 toJSON(scores_df, pretty=TRUE) 
